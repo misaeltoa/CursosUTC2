a91a17bca327b4e7989115d971cb45788195bba0
Misael Torres <23040025@alumno.utc.edu.mx>
2025-10-09 18:45:16 -0600

    se subieron las dos carpetas al rep. local

---
+++ practica3/src/app/practica3/practica3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practica3',
+  imports: [],
+  templateUrl: './practica3.html',
+  styleUrl: './practica3.css'
+})
+export class Practica3 {
+
+}
