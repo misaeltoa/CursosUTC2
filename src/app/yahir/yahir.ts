18fc2a64c375a06657c9e064e8b81663d53943dc
Yahir Martinez <23040094@alumno.utc.edu.mx>
2025-10-16 18:20:50 -0600

    Agrego componente Yahir funcional

---
+++ src/app/yahir/yahir.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yahir',
+  standalone: true,
+  templateUrl: './yahir.html',
+  styleUrl: './yahir.scss'
+})
+export class Yahir {
+  mensaje = 'Este es mi primer componente realizado en Angular';
+}
