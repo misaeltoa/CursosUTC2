fb5df0bfef73f5f6481d2351a23258e5e888b67c
Yahir Martinez <23040094@alumno.utc.edu.mx>
2025-10-29 21:19:59 -0600

    Limpieza: solo carpeta de Sergio Luis

--- src/app/pages/jonathan-alexis/jonathan-alexis.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-jonathan-alexis',
-  templateUrl: './jonathan-alexis.component.html',
-  styleUrls: ['./jonathan-alexis.component.scss']
-})
-export class JonathanAlexisComponent {}
