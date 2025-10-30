f21970321af2856fb4ec4da192107d6322e15973
Sergio Davila <sergiodavilatovar@gmail.com>
2025-10-30 13:56:27 -0600

    Listo para Merge

--- src/app/pages/sergio-luis/sergio-luis.component.ts
+++ src/app/pages/sergio-luis/sergio-luis.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sergio-luis',
+  standalone: true,
   templateUrl: './sergio-luis.component.html',
   styleUrls: ['./sergio-luis.component.scss']
 })
-export class SergioLuisComponent {}
+export class SergioLuisComponent {
+  currentYear = new Date().getFullYear();
+}
