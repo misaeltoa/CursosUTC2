7bb900e5d750a23cd11512d95215b84618254007
Yahir Martinez <23040094@alumno.utc.edu.mx>
2025-10-29 21:19:38 -0600

    Limpieza: solo carpeta de Yahir Martinez

--- src/app/pages/saul-fabian/saul-fabian.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-saul-fabian',
-  templateUrl: './saul-fabian.component.html',
-  styleUrls: ['./saul-fabian.component.scss']
-})
-export class SaulFabianComponent {}
