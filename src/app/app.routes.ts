d406a257bbc764d377fae3646e77a19a60482700
Yahir Martinez <23040094@alumno.utc.edu.mx>
2025-11-06 15:37:53 -0600

    Agregar componente Yahir Alejandro y actualizar index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { IndexComponent } from './pages/index/index.component';
 import { SergioLuisComponent } from './pages/sergio-luis/sergio-luis.component';
 import { JonathanAlexisComponent } from './pages/jonathan-alexis/jonathan-alexis.component';
 import { YahirMartinezComponent } from './pages/yahir-martinez/yahir-martinez.component';
+import { YahirAlejandroComponent } from './pages/yahir-alejandro/yahir-alejandro.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
@@ -10,5 +12,6 @@ export const routes: Routes = [
   { path: 'sergio-luis', component: SergioLuisComponent },
   { path: 'jonathan-alexis', component: JonathanAlexisComponent },
   { path: 'yahir-martinez', component: YahirMartinezComponent },
+  { path: 'yahir-alejandro', component: YahirAlejandroComponent },
   { path: '**', redirectTo: 'index' } // Ruta por defecto
 ];
