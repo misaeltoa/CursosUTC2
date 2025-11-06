2e6ca55f7e06b1af99485f028a2df5a0560736b8
ALEXTINTOR <23040027@alumno.utc.edu.mx>
2025-11-06 02:20:28 -0600

    fix: Arregla los conflictos de fusión que se subieron mal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-<<<<<<< HEAD
 import { YahirAlejandroComponent } from './pages/yahir-alejandro/yahir-alejandro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, YahirAlejandroComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
-=======
-@Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, YahirAlejandroComponent], 
   templateUrl: './app.html',
-  styleUrls: ['./app.scss'], // <-- corregido
->>>>>>> 76caefd62f2593b1dd68d9b66b32ca640fcd645d
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('app-angular');
