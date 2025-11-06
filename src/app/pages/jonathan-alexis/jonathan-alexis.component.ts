6faa7cac6cf69bddb66ec709ae3025a5136dba85
Yahir Martinez <23040094@alumno.utc.edu.mx>
2025-11-05 20:47:45 -0600

    Agregar componente Jonathan Alexis

---
+++ src/app/pages/jonathan-alexis/jonathan-alexis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-jonathan-alexis',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './jonathan-alexis.component.html',
+  styleUrls: ['./jonathan-alexis.component.scss']
+})
+export class JonathanAlexisComponent {}
