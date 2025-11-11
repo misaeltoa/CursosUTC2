cd55d3f1bc80beb536e7da45d5a212e0603fdde0
ALEXTINTOR <23040027@alumno.utc.edu.mx>
2025-11-11 14:06:40 -0600

    feat: Migra el componente de perfil de Yahir Alejandro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './pages/index/index.component';
-import { SergioLuisComponent } from './pages/sergio-luis/sergio-luis.component';
-import { JonathanAlexisComponent } from './pages/jonathan-alexis/jonathan-alexis.component';
-import { YahirMartinezComponent } from './pages/yahir-martinez/yahir-martinez.component';
+// import { IndexComponent } from './pages/index/index.component';
+// import { SergioLuisComponent } from './pages/sergio-luis/sergio-luis.component';
+// import { JonathanAlexisComponent } from './pages/jonathan-alexis/jonathan-alexis.component';
+// import { YahirMartinezComponent } from './pages/yahir-martinez/yahir-martinez.component';
+// import { CandidoMisaelComponent } from './pages/candido-misael/candido-misael.component';
+
 import { YahirAlejandroComponent } from './pages/yahir-alejandro/yahir-alejandro.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
-  { path: 'index', component: IndexComponent },
-  { path: 'sergio-luis', component: SergioLuisComponent },
-  { path: 'jonathan-alexis', component: JonathanAlexisComponent },
-  { path: 'yahir-martinez', component: YahirMartinezComponent },
-  { path: 'yahir-alejandro', component: YahirAlejandroComponent },
-  { path: '**', redirectTo: 'index' }
+  // { path: '', redirectTo: 'index', pathMatch: 'full' },
+  // { path: 'index', component: IndexComponent },
+  // { path: 'sergio-luis', component: SergioLuisComponent },
+  { path: 'alejandro', component: YahirAlejandroComponent },
+  { path: '', redirectTo: 'alejandro', pathMatch: 'full' }
 ];
