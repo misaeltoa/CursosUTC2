de7cce406f0d5f249e2197199adfd212d6210cb0
ALEXTINTOR <23040027@alumno.utc.edu.mx>
2025-11-26 15:19:42 -0600

    fix: Arreglar archivos app.routes.ts, app.ts y mi nombre en index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { SergioLuisComponent } from './pages/sergio-luis/sergio-luis.component';
 import { JonathanAlexisComponent } from './pages/jonathan-alexis/jonathan-alexis.component';
 import { YahirMartinezComponent } from './pages/yahir-martinez/yahir-martinez.component';
 import { YahirAlejandroComponent } from './pages/yahir-alejandro/yahir-alejandro.component';
+import { SaulFabianComponent } from './pages/saul-fabian/saul-fabian.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
-  { path: 'index', component: IndexComponent },
-  { path: 'sergio-luis', component: SergioLuisComponent },
-  { path: 'jonathan-alexis', component: JonathanAlexisComponent },
-  { path: 'yahir-martinez', component: YahirMartinezComponent },
-  { path: 'yahir-alejandro', component: YahirAlejandroComponent },
-  { path: '**', redirectTo: 'index' }
+    {path: '', component: IndexComponent},
+    {path: 'index', component: IndexComponent},
+    {path: 'sergio-luis', component: SergioLuisComponent},
+    {path: 'jonathan-alexis', component: JonathanAlexisComponent},
+    {path: 'yahir-martinez', component: YahirMartinezComponent},
+    {path: 'yahir-alejandro', component: YahirAlejandroComponent},
+    {path: 'saul-fabian', component: SaulFabianComponent},
+    
+    {path: '**', redirectTo: 'index'}
 ];
